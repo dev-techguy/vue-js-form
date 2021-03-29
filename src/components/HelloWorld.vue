710021d12bc3e1974e01d06ea23832f4fb5b97a9
techguy <dev.techguy@gmail.com>
2021-03-29 10:08:18 +0300

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,46 +11,52 @@
           <div class="form-group">
             <label>Full Name</label>
             <input type="text" placeholder="Full Name" v-model="details.name" required class="form-control">
+            <p class="text-danger" v-if="!$v.name.required">The name field is required!</p>
           </div>
           <div class="form-group">
             <label>Email Address</label>
             <input type="email" placeholder="Email Address" v-model="details.email" required class="form-control">
-<!--            <div class="text-sm mt-2 text-red" v-if="v.$error">-->
-<!--              <div v-if="!v.required">Email is required</div>-->
-<!--              <div v-if="!v.email">Email is not a properly formatted email address</div>-->
-<!--            </div>-->
+            <p class="text-danger" v-if="!$v.email.required">The email field is required!</p>
+            <p class="text-danger" v-if="!$v.email.email">The input must be a proper email!</p>
           </div>
           <div class="form-group">
             <label>Date Of Birth</label>
             <input type="date" placeholder="DOB" v-model="details.date_of_birth" required class="form-control">
+            <p class="text-danger" v-if="!$v.date_of_birth.required">The dob field is required!</p>
           </div>
           <div class="form-group">
             <label>Home Address</label>
             <input type="text" placeholder="Home address" v-model="details.home_address" required class="form-control">
+            <p class="text-danger" v-if="!$v.home_address.required">The home address field is required!</p>
           </div>
           <div class="form-group">
             <label>School Attended</label>
             <input type="text" placeholder="School Attended" v-model="details.school_attended" required
                    class="form-control">
+            <p class="text-danger" v-if="!$v.school_attended.required">The school attended field is required!</p>
           </div>
           <div class="form-group">
             <label>Year Finished School</label>
             <input type="number" placeholder="Year Finished School" v-model="details.year_finished_school" required
                    class="form-control">
+            <p class="text-danger" v-if="!$v.year_finished_school.required">The year of finish field is required!</p>
           </div>
           <div class="form-group">
             <label>Grades Achieved</label>
             <input type="number" placeholder="Grades Achieved" v-model="details.grades_achieved" required
                    class="form-control">
+            <p class="text-danger" v-if="!$v.grades_achieved.required">The grades field is required!</p>
           </div>
           <div class="form-group">
             <label>Courses</label>
             <select v-model="details.courses" required class="form-control">
+              <option value="courses" disabled selected>Select Courses</option>
               <option value="postgresql">PostgresSQL</option>
               <option value="spring_boot">Spring Boot</option>
               <option value="nodejs">NodeJs</option>
               <option value="laravel">Laravel</option>
             </select>
+            <p class="text-danger" v-if="!$v.courses.required">The courses field is required!</p>
           </div>
           <div class="form-group">
             <button class="btn btn-primary float-right">SUBMIT</button>
